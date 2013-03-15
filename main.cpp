4ac68a8fd0ce88a1a1dd60eb7fa95653fdfe10fe
eldhom <wass95@hotmail.se>
2013-03-15 13:40:59 +0100

    lol2

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ int main()
     {
         char cmd;
         cout<<"Personality Generator"<<endl;
-        cout<<setw(6)<<setfill('-')<<left<<"1"<<"Add Person"<<endl;
-        cout<<setw(6)<<"2"<<"Remove Person"<<endl;
-        cout<<setw(6)<<"3"<<"Edit Person"<<endl;
+        cout<<setw(3)<<setfill('-')<<left<<"1"<<setw(20)<<right<<"Add Person"<<endl;
+        cout<<setw(3)<<left<<"2"<<setw(20)<<right<<"Remove Person"<<right<<endl;
+        cout<<setw(3)<<left<<"3"<<setw(20)<<right<<"Edit Person"<<endl;
         cin>>cmd;
         if(cmd==1) {
             //Person temp;
