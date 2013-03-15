c56611df69f0e7c9404b6e5701dbccb8fc30dee1
eldhom <wass95@hotmail.se>
2013-03-15 13:09:36 +0100

    lol2

---
+++ Person.hpp
@@ -0,0 +1,15 @@
+#ifndef PERSON_HPP
+#define PERSON_HPP
+
+enum sor { SET, RANDOM };
+
+class Person {
+
+    string              mentil;
+
+public:
+    void setPersonality(sor choice);
+};
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+#include"Person.hpp"
+int main()
+{
+//    vector<Person>persons;
+    bool run=true;
+    while(run)
+    {
+        char cmd;
+        cout<<"Personality Generator"<<endl;
+        cout<<"1"<<setw(6)<<"Add Person"<<endl;
+        cout<<"2"<<setw(6)<<"Remove Person"<<endl;
+        cout<<"3"<<setw(6)<<"Edit Person"<<endl;
+        cin>>cmd;
+        if(cmd==1) {
+            //Person temp;
+            //persons.push_back(temp);
+        }
+    }
+    
+}
