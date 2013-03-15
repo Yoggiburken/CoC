47a97a893f06ea62f9ba8b36af1b55c308d542b8
eldhom <wass95@hotmail.se>
2013-03-15 13:43:23 +0100

    lol2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 #include"Person.hpp"
 int main()
 {
-//    vector<Person>persons;
     bool run=true;
     while(run)
     {
@@ -13,10 +12,7 @@ int main()
         cout<<setw(3)<<setfill('-')<<left<<"1"<<setw(20)<<right<<"Add Person"<<endl;
         cout<<setw(3)<<left<<"2"<<setw(20)<<right<<"Remove Person"<<right<<endl;
         cout<<setw(3)<<left<<"3"<<setw(20)<<right<<"Edit Person"<<endl;
+        cout
         cin>>cmd;
-        if(cmd==1) {
-            //Person temp;
-            //persons.push_back(temp);
-        }
     }    
 }
