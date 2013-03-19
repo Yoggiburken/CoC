a1b4f1c96b2208cddfd71be378f406e8d72ba676
eldhom <wass95@hotmail.se>
2013-03-19 10:25:53 +0100

    lol

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
-#include"Persons.hpp"
+#include"Person.hpp"
+#include<vector>
 int main()
 {
+    vector<Person>persons;
     bool run=true;
     while(run)
     {
@@ -12,6 +14,8 @@ int main()
         cout<<setw(3)<<setfill('-')<<left<<"1"<<setw(20)<<right<<"Add Person"<<endl;
         cout<<setw(3)<<left<<"2"<<setw(20)<<right<<"Remove Person"<<right<<endl;
         cout<<setw(3)<<left<<"3"<<setw(20)<<right<<"Edit Person"<<endl;
+        cout<<endl<<setw(3)<<left<<"9"<<setw(20)<<right<<"Start Playing"<<endl;
+        cout<<setw(3)<<left<<"0"<<setw(20)<<right<<"Exit"<<endl;
         cin>>cmd;
     }    
 }
